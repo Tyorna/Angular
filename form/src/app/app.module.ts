4294c2c7accccaaef7cad0bbd29c1940e7495338
Tyorna <francescaarata@live.it>
2023-06-06 17:04:07 +0200

    esempii

--- form/src/app/app.module.ts
+++ form/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SimpleformComponent } from './simpleform/simpleform.component';
@@ -12,7 +13,8 @@ import { SimpleformComponent } from './simpleform/simpleform.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- form/src/app/simpleform/simpleform.component.ts
+++ form/src/app/simpleform/simpleform.component.ts
@@ -1,67 +1,40 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
-
-@Component({
+import { Component, OnInit } from '@angular/core';
+  import {FormBuilder, FormControl, FormGroup, Validators, FormArray,} from '@angular/forms';
+  @Component({
   selector: 'app-simpleform',
   templateUrl: './simpleform.component.html',
   styleUrls: ['./simpleform.component.scss']
 })
-export class SimpleformComponent implements OnInit {
-  @ViewChild('contenitoreForm', {static: true}) contenitoreForm!: NgForm; // Con ViewChild static:true si recepisce il cambiamento dello stato del child indicato PRIMA dello stato afterViewInit, quindi anche se in fase di controllo il valore nel template cambia, il valore nel component rimane comunque allineato; con la classe NgForm gestiamo il form come un'unica istanza formGroup, quindi controlliamo i diversi valori nel form come elementi di un'unica istanza
+  export class SimpleformComponent implements OnInit {
 
-  userForm = {
-      defUserName: '',
-      email: '',
-      question: ''
-  }
+      contenitoreForm!: FormGroup;
 
-  generi = [
-      {
-          label: 'uomo',
-          value: 'uomo'
-      },
-      {
-          label: 'donna',
-          value: 'donna'
+      constructor(private fb: FormBuilder) {}
+
+      ngOnInit(): void {
+          };
+
+      getErrorsC(name: string, error: string) {
+          return this.contenitoreForm.get(name)?.errors![error];
       }
-  ]
 
-  risposta = '';
+      getFormC(name: string) {
+          return this.contenitoreForm.get(name);
+      }
 
-  user: any = {
-      username: '',
-      email: '',
-      secret: '',
-      risposta: '',
-      sesso: ''
+      getPowers() {
+          return (this.contenitoreForm.get('powers') as FormArray).controls;
+      }
+
+      addPower() {
+          const control = this.fb.control(null);
+          (this.contenitoreForm.get('sports') as FormArray).push(control);
+          console.log(this.getPowers());
+      }
+
+      submitForm() {
+          console.log(this.contenitoreForm);
+          console.log('Form correttamente inviato');
+          this.contenitoreForm.reset();
+      }
   }
-  constructor() { }
-
-  ngOnInit(): void {
-    this.contenitoreForm.statusChanges?.subscribe(stato => {
-      console.log(this.contenitoreForm);
-      console.log(`Stato del form: ${stato}`);
-  });
-  }
-
-  generateUsername() {
-    this.contenitoreForm.form.patchValue({
-        userInfo: {
-            email: 'pippo@pippo.com',
-            username: 'pippo'
-        }
-    });
-}
-
-submitForm() {
-    console.log(`Form inviato: ${this.contenitoreForm}`);
-    this.user.username = this.contenitoreForm.value.userInfo.username;
-    this.user.email = this.contenitoreForm.value.userInfo.email;
-    this.user.secret = this.contenitoreForm.value.userInfo.secret;
-    this.user.risposta = this.contenitoreForm.value.userInfo.risposta;
-    this.user.sesso = this.contenitoreForm.value.userInfo.sesso;
-
-    this.contenitoreForm.reset();
-}
-
-}
